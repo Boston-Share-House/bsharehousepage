91b2fe8fb1872c9d95aaf8749ea1e13f9e2e2b8a
Rikuo Hasegawa <rikuo.hase1997@gmail.com>
2017-02-21 01:16:32 -0500

    Setup django project

--- app.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-const Console = console;
-
-app.use(express.static('public'))
-
-app.listen(80, _  => Console.log('FUCK YEAH LET\'S GO'))
---
+++ bsharehouse/settings/prod.py
@@ -0,0 +1,26 @@
+from bsharehouse.settings.common import *
+
+DEBUG = False
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Link the S3 instance
+INSTALLED_APPS += ('storages',)
+AWS_STORAGE_BUCKET_NAME = "bsharehouses3"
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+S3_URL = 'http://%s.s3.amazonaws.com/' % AWS_STORAGE_BUCKET_NAME
+STATIC_URL = S3_URL
+
+# Link the Postgres DB
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'bsharehousedb',
+        'USER' : 'bsharehouse',
+        'PASSWORD' : r"M*%zSzz79C!2",
+        'HOST' : 'bsharehousedb.convlvgvotec.us-west-2.rds.amazonaws.com',
+        'PORT' : '5432',
+    }
